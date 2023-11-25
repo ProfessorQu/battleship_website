36422c37fd518d763d675f9473cd767e65efeffd
ProfessorQu <quint@dasookwat.com>
2023-11-25 21:58:38 +0100

    Generated boats.

--- src/game.rs
+++ src/game.rs
@@ -8,7 +8,17 @@ pub fn generate_boats(recording: UseStateHandle<Option<Recording>>) -> Html {
 
     let recording = recording.as_ref().expect("How");
 
-    let boats: Html = recording.player1_boats.iter().map(|row| {
+    let player1_boats: Html = recording.player1_boats.iter().map(|row| {
+        let row: Html = row.iter().map(|item| {
+            html!(<td class={ "boat".to_string() + &(*item as usize).to_string() }></td>)
+        }).collect();
+
+        html!(
+            <tr>{row}</tr>
+        )
+    }).collect();
+
+    let player2_boats: Html = recording.player2_boats.iter().map(|row| {
         let row: Html = row.iter().map(|item| {
             html!(<td class={ "boat".to_string() + &(*item as usize).to_string() }></td>)
         }).collect();
@@ -19,8 +29,18 @@ pub fn generate_boats(recording: UseStateHandle<Option<Recording>>) -> Html {
     }).collect();
 
     html!(
-        <table class="boats">
-            { boats }
-        </table>
+        <>
+            { "Boats" }
+            <br />
+            <table class="boats">
+                <tr>{ "P1" }</tr>
+                { player1_boats }
+            </table>
+            <span class="tab" />
+            <table class="boats">
+                <tr>{ "P2" }</tr>
+                { player2_boats }
+            </table>
+        </>
     )
 }
